c5af951850cc8cd56187aa0b02f1b3b2d543cf60
ddiske <ddiske@naver.com>
2024-12-27 17:49:01 +0900

    20241227Last

--- src/com/root/app/controls/StudyLoop9.java
+++ src/com/root/app/controls/StudyLoop9.java
@@ -26,79 +26,58 @@ public class StudyLoop9 {
 	 */
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("프로그램 시작");
-		
-//		boolean login = true;
-//		int lv = 1;
-//		int exp = 0;
-//		int gold = 0;
-//		int nexp = lv * 3;
-//		
-//		
-//		
-//		while(login) {
-//			System.out.println("1.사냥하러가기 2.게임종료");
-//			int select = sc.nextInt();
-//			
-//			if(select == 1) {
-//				for(lv = 1; lv < 15; lv++) {
-//					nexp = lv * 3;
-//
-//					for(exp = 1; exp < nexp + 1; exp++) {
-//						System.out.println(exp + "경험치 획득");
-//						if(exp == nexp) {
-//							System.out.println(lv + 1 + "레벨 달성");
-//						}
-//						
-//					}
-//					if(lv == 5) {
-//						gold = gold + 1000;
-//						System.out.println(gold + "골드를 획득");
-//					}else if(lv == 10) {
-//						gold = gold + 2000;
-//						System.out.println(gold + "골드를 획득");
-//					}else if(lv == 15) {
-//						gold = gold + 3000;
-//						System.out.println(gold + "골드를 획득");
-//					}
-//				}
-//				
-//				
-//			}else if( select == 2) {
-//				System.out.println("게임 종료");
-//			}else {
-//				System.out.println("다시 입력하세요");
-//			}
-//		}
 		
 		int level = 1;
 		int gold = 0;
 		
 		for(level = 1; level < 15; level++) {
-			System.out.println("1.사냥 2.종료");
+			System.out.println("1.시작 2.종료");
 			int select = sc.nextInt();
-			if(select != 1) {
-				break;
-			}
 			
-			for(int mon = 0; mon < level*3; mon++) {
-				System.out.println(mon+1 + "마리 사냥 성공");
-			}
-			
-			System.out.println("축 레벨업!!");
-			
-			if((level+1) % 5 == 0) {
-				gold = gold + (level+1)/5*1000;
+			if(select == 1) {
+				for(int mon = 0; mon < level * 3; mon++) {
+					System.out.println(mon+1 + "경험치 획득");
+				}
+				System.out.println(level+1 + "레벨 달성");
+				if((level+1) % 5 == 0) {
+					gold = (level+1)*200;
+					System.out.println(gold + "골드 획득");
+				}
 			}
 		}
 		
-		System.out.println("현재 레벨 : " + level);
-		System.out.println("현재 골드 : " + gold);
+		
+		
+//		int level = 1;
+//		int gold = 0;
+//		
+//		for(level = 1; level < 15; level++) {
+//			System.out.println("1.사냥 2.종료");
+//			int select = sc.nextInt();
+//			if(select != 1) {
+//				break;
+//			}
+//			
+//			for(int mon = 0; mon < level*3; mon++) {
+//				System.out.println(mon+1 + "마리 사냥 성공");
+//			}
+//			
+//			System.out.println("축 레벨업!!");
+//			
+//			if((level+1) % 5 == 0) {
+//				gold = gold + (level+1)/5*1000;
+//			}
+//		}
+//		
+//		System.out.println("현재 레벨 : " + level);
+//		System.out.println("현재 골드 : " + gold);
 		
 
 		
-
+		System.out.println("프로그램 종료");
 	}
 }
 
