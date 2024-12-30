4f7922d721655d5a3036ff3f07b7030884056fed
ddiske <ddiske@naver.com>
2024-12-30 11:52:09 +0900

    Day5_3

---
+++ src/com/root/app/study1/array/ArrayStudy3.java
@@ -0,0 +1,22 @@
+package com.root.app.study1.array;
+
+public class ArrayStudy3 {
+	
+	
+
+	public static void main(String[] args) {
+		//정수 2개를 모을 배열 선언
+		//모을려고 하는 데이터타입 [] 변수명
+		int [] nums = {10, 20};
+		
+		int ar [] = new int [3];
+		
+		nums = ar;
+		
+		ar = nums;
+		
+		ar[0];
+
+	}
+
+}
